6499c4a51894ba05e877e281e296c2452bffe642
UserShri98 <sjc3098@gmail.com>
2026-01-14 10:29:06 +0530

    changed backend url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:2000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function getDashboardSummary() {
   const res = await fetch(`${BASE_URL}/dashboard/summary`);
